142a26d10ea2c27f8f8cc4fbc2dfc88cee38fe5f
Kurt Rose <kurt@kurtrose.com>
2025-08-12 23:45:06 -0700

    Add module validation pipeline

--- __macrotype__/macrotype/modules/__init__/__init__.pyi
+++ __macrotype__/macrotype/modules/__init__/__init__.pyi
@@ -1,2 +1,17 @@
-# Generated via: macrotype macrotype
-# Do not edit by hand
+from types import ModuleType
+from typing import Any, Mapping
+
+from .emit_module import emit_module
+from .scanner import ModuleInfo, scan_module
+from .validate import ModuleValidationError, validate
+
+def from_module(glb_or_mod: ModuleType | Mapping[str, Any]) -> ModuleInfo: ...
+
+__all__ = [
+    "ModuleInfo",
+    "from_module",
+    "emit_module",
+    "scan_module",
+    "validate",
+    "ModuleValidationError",
+]
